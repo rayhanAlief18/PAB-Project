58d03b25834aa29f3cfaa1cf8de71065f4e5d3ff
azhim <azhimdiendro2@gmail.com>
2026-01-07 16:53:21 +0700

    Pindahkan Task Summary dari Money Track ke To Do List

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,18 +1,18 @@
 import { Header } from '@/components/custom';
 import CardGroupTask from '@/components/custom/Card/CardGroupTask';
-import TodayTask from '@/components/custom/Card/TodayTask';
 import MenuToDoList from '@/components/custom/Menu/MenuToDoList';
+import TaskSummary from '@/components/custom/ToDo/TaskSummary';
 import { ScrollView } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 export default function TabTwoScreen() {
   return (
 
     <SafeAreaView className='flex-1 bg-[#F2F2F7] pb-[-20px]'>
-      <Header name='Azhim' customClass={'px-[30px]'}/>
+      <Header name='Azhim' customClass={'px-[30px]'} />
       <ScrollView className='-mb-[30px]'>
-        <MenuToDoList customClass={'px-[30px] mt-[12px]'}/>
-        <CardGroupTask  />
-        <TodayTask customClass={'px-[30px]'} />
+        <MenuToDoList customClass={'px-[30px] mt-[12px]'} />
+        <CardGroupTask />
+        <TaskSummary customClass='px-[30px] mt-[20px]' />
       </ScrollView>
     </SafeAreaView>
   );
